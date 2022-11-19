5d3542f8d1382444446436abb8aa87296b7c7339
David Marie Jeanne <david.j.alexis11@gmail.com>
2022-11-19 22:00:29 +0400

    Registration form completed (64)

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .forms import RegistrationForm
+
+def register(request):
+    form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    return
 
 # Create your views here.
